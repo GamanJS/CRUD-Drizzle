130201ca11a749615e23620c0aef4cb4d8fe5c1d
Angga7Togk <kiplihode123321@gmail.com>
2025-10-25 23:01:09 +0700

    feat: remove http exception

--- src/exceptions/BlogException.ts
+++ src/exceptions/BlogException.ts
@@ -2,9 +2,7 @@ import { HttpException } from '@gaman/common';
 import { composeExceptionHandler } from '@gaman/core';
 
 export default composeExceptionHandler((err) => {
-	if (err instanceof HttpException) {
-		return Res.internalServerError({
+	return Res.internalServerError({
 			message: 'Internal Server Error',
 		});
-	}
 });
